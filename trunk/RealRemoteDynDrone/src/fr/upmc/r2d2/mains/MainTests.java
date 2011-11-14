201e259685990685042685bbb824bdad66641b33
hebert.alex@gmail.com <hebert.alex@gmail.com@76eb45ce-1650-7903-4311-20ba978306c7>
2011-11-14 19:16:11 +0000

    Intégration des Tests

--- trunk/RealRemoteDynDrone/src/fr/upmc/r2d2/mains/MainTests.java
+++ trunk/RealRemoteDynDrone/src/fr/upmc/r2d2/mains/MainTests.java
@@ -4,11 +4,18 @@ import fr.upmc.dtgui.example.World;
 import fr.upmc.dtgui.tests.AnotherLittleRobot;
 import fr.upmc.dtgui.tests.LittleRobot;
 import fr.upmc.dtgui.robot.Robot;
+import fr.upmc.r2d2.boards.DynGUI;
+import fr.upmc.r2d2.tools.TestDetails;
 import fr.upmc.r2d2.tools.Utils;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,8 +27,14 @@ import org.junit.Test;
 public class MainTests {
     
     private Class<? extends Robot>[] robotsClassLoader;
-    private Class<World> word;
-    private Robot first, another;
+    private Robot[] robots = new Robot[2];
+    private Class<World> wordClassLoader;
+    private Class<DynGUI> guiClassLoader;
+    
+    private static final int FIRST = 0, ANOTHER = 1;
+    
+    private static final Map<Integer, Entry<TestDetails, Method>> testsMethods = new HashMap();
+    private static int testsOrder = 0;
     
     public static void main(String... args) throws Exception {
         // petit bidule pour tout de même évaluer les tests junit malgré javassist
@@ -29,20 +42,34 @@ public class MainTests {
         mainTests.setUp();
         
         Utils.block("Tests", new Utils.Block() {
-
+            
             @Override
             public void run() {
 
-                for (Method m : MainTests.class.getMethods())
-                    for (Annotation a : m.getAnnotations())
-                        if (a instanceof Test)
-                            try {
-                                m.invoke(mainTests);
-                                System.out.println("> " + m.getName() + " success");
-                            } catch (Exception e) {
-                                System.out.println("> " + m.getName() + " error : " + e.getCause().getClass().getSimpleName() + " " + e.getCause().getMessage());
-                                e.printStackTrace();
-                            }
+                for (Method m : MainTests.class.getMethods()) {
+                    for (Annotation a : m.getAnnotations()) {
+                        if (a instanceof TestDetails)
+                            testsMethods.put(((TestDetails)a).order(), new HashMap.SimpleEntry(a, m));
+                    }
+                }
+                
+                for (Object o : (new TreeMap(testsMethods)).entrySet()) {
+                    Entry<TestDetails, Method> v = (Entry<TestDetails, Method>) ((Entry) o).getValue();
+                    Method m = v.getValue();
+                    TestDetails td = v.getKey();
+                    try {
+                        go(m.getName(), td.description());
+                        m.invoke(mainTests);
+                        pass();
+                    } catch (Exception e) {
+                        err(e.getCause().getClass().getSimpleName() + " " + e.getCause().getMessage());
+                    } finally {
+                        print();
+                        flush();
+                    }
+                }
+                
+                print(">>> nombre d'erreurs : " + errors());
                 
             }
             
@@ -58,32 +85,59 @@ public class MainTests {
     @Before
     public void setUp() throws Exception {
         robotsClassLoader = new Class[] {LittleRobot.class, AnotherLittleRobot.class};
-        word = World.class;
+        guiClassLoader = DynGUI.class;
+        wordClassLoader = World.class;
+        robots[FIRST] = (Robot) robotsClassLoader[FIRST].getConstructors()[0].newInstance("No 5", 2000.0, 950.0, 45.0);
+        robots[ANOTHER] = (Robot) robotsClassLoader[ANOTHER].getConstructors()[0].newInstance("No 1", 2850.0, 950.0, 135.0, 10.0);
     }
     
     @After
     public void tearDown() throws Exception {}
     
     @Test
-    public void testRobots() throws Exception {
-        first = (Robot) robotsClassLoader[0].getConstructors()[0].newInstance("No 5", 2000.0, 950.0, 45.0);
-        another = (Robot) robotsClassLoader[1].getConstructors()[0].newInstance("No 1", 2850.0, 950.0, 135.0, 10.0);
+    @TestDetails(order=0,description="XXXRobot implements InstrumentedRobot ?")
+    public void testRobotsInheritance() throws Exception {
+        
         Class<?> instrumented = Class.forName("fr.upmc.dtgui.robot.InstrumentedRobot");
-        assert (Arrays.asList(first.getClass().getInterfaces()).contains(instrumented));
-        assert (Arrays.asList(another.getClass().getInterfaces()).contains(instrumented));
+        
+        for (Robot r : robots) {
+            assert (Arrays.asList(r.getClass().getInterfaces()).contains(instrumented));
+            System.out.println("\t" + r.getRobotName() + " implémente instrumented");
+        }
+        
     }
     
     @Test
+    @TestDetails(order=3,description="XXXSensorDataSender créé ?")
     public void testSensorDataSender() throws Exception {
-        Class<?> c = Class.forName("fr.upmc.dtgui.tests.FirstLittleRobotSensorDataSender");
-        Object sds = c.getConstructors()[0].newInstance(first);
-        System.out.println("> methods");
-        for (Method m : sds.getClass().getMethods())
-            if (m.getName().equals("run") || m.getName().equals("start"))
-                System.out.println("\t" + m.getName());
-        System.out.println("> fields");
-        for (Field f : sds.getClass().getDeclaredFields())
-            System.out.println("\t" + f.getName());
+        
+        for (Robot r : robots) {
+            
+            Class<?> c = Class.forName(r.getClass().getName() + MainJavassist.RobotMaker.SENDER_EXT);
+            Object sds = c.getConstructors()[0].newInstance(r);
+            System.out.println("class " + c.getSimpleName() + " :");
+            System.out.print("\t> methods :");
+            for (Method m : sds.getClass().getMethods())
+                if (m.getName().equals("run") || m.getName().equals("start"))
+                    System.out.print(" " + m.getName());
+            System.out.println();
+            System.out.print("\t> fields :");
+            for (Field f : sds.getClass().getDeclaredFields())
+                System.out.print(" " + f.getName());
+            System.out.println();
+            
+        }
+        
+    }
+    
+    @Test
+    @TestDetails(order=2,description="Queues de communication fonctionnelles ?")
+    public void testQueues() throws Exception {
+        
+        Class<?> c = Class.forName(robots[0].getClass().getName() + MainJavassist.RobotMaker.SENDER_EXT);
+        System.out.println("instanciation de " + c.getSimpleName());
+        c.getConstructors()[0].newInstance(robots[0]);
+        
     }
     
 }
